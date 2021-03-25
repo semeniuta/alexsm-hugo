51d7d4af3aca9504d131f7a7f0618c88fb513584
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-03-25 20:09:19 +0100

    Change logic in deploy.sh (hugo_public)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -avz ./ alex@alexsm.com:/home/alex/alexsm/hugo
+# Regenerate files in public
+
+rm -rf public/*
+hugo
+
+# Copy files from the public directory 
+# to ~/alexsm/hugo_public of the server
+
+rsync -avz public/ alex@alexsm.com:/home/alex/alexsm/hugo_public
