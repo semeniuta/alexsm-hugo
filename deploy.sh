c01137fb6d62064f70e307a687ef5942843727bf
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2022-11-25 14:41:20 +0100

    deploy.sh: generate to tmp dir

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# Regenerate files in public
+# Create a temporary directory
+temp_public_dir="/tmp/hugo_public_$RANDOM"
+mkdir $temp_public_dir
 
-rm -rf public/*
-hugo
+# Generate site
+hugo -d $temp_public_dir
+
+# Sync the changes with the local public directory
+rsync -tdvz --delete $temp_public_dir/ public
 
 # Copy files from the public directory 
 # to ~/alexsm/hugo_public of the server
+rsync -avz --delete $temp_public_dir/ alex@alexsm.com:/home/alex/alexsm/hugo_public
 
-rsync -avz --delete public/ alex@alexsm.com:/home/alex/alexsm/hugo_public
+# Remove the temporary directory
+rm -rf $temp_public_dir
